7a2451f0b5ee935d843f4b62cc92b16fe113581d
Ethan Alon <ethan.alon@kin-keepers.com>
2023-06-13 10:35:13 -0700

    work on nav bar

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function copyColor(color){
     navigator.clipboard.writeText(color);
     alert("Copied the color: " + color)
 }
+
+$(document).ready(function() {
+    $('.toggle').click(function() {
+      $('.menu').toggleClass('active');
+    });
+  });
