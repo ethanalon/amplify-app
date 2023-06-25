cfe6e45cb0c4191d0b830fe255f33c1899f162db
Ethan Alon <ethanalon@gmail.com>
2023-06-24 22:11:02 -0700

    RSVP fixed rsvp page flow added google sheets form submission
    *
    changed main font to Ysabeau Infant

--- confirmation.js
+++ confirmation.js
@@ -1,25 +1,9 @@
-// Function to extract URL parameters
-function getURLParameters() {
-    var searchParams = new URLSearchParams(window.location.search);
-    var params = {};
-  
-    for (var param of searchParams) {
-      params[param[0]] = param[1];
-    }
-  
-    return params;
-  }
-  
-  // Function to populate the confirmation message
-  function populateConfirmationMessage() {
-    var parameters = getURLParameters();
-    var name = parameters['name'];
-    var phone = parameters['phone'];
-  
-    var confirmationMessage = document.getElementById('confirmation-message');
-    confirmationMessage.innerHTML = 'Thank you, ' + name + ', for your submission! We will contact you at ' + phone + ' shortly.';
-  }
-  
-  // Call the function to populate the confirmation message
-  populateConfirmationMessage();
-  
+
+
+var name = document.getElementById('name').value;
+  var phone = document.getElementById('phone').value;
+  var email = document.getElementById('email').value;
+
+
+var confirmationMessage = document.getElementById('confirmation-message');
+  confirmationMessage.innerHTML = 'Thank you, ' + name + ', for your submission! We will contact you at ' + phone + ' shortly.';
--- script.js
+++ script.js
@@ -1,92 +1,123 @@
-// Variables
-var questions = document.querySelectorAll('.question');
-var currentQuestionIndex = 0;
-var form = document.getElementById('questionForm');
-var nextButton = document.getElementById('nextButton');
-var prevButton = document.getElementById('prevButton');
+// // Variables
+// var questions = document.querySelectorAll('.question');
+// var currentQuestionIndex = 0;
+// var form = document.getElementById('questionForm');
+// var nextButton = document.getElementById('nextButton');
+// var prevButton = document.getElementById('backButton');
+// // var questionIndexDisplay = document.getElementById('questionIndexDisplay');
 
-// Function to show the next question
-function showNextQuestion() {
-  if (currentQuestionIndex < questions.length - 1) {
-    questions[currentQuestionIndex].classList.remove('active');
-    currentQuestionIndex++;
-    questions[currentQuestionIndex].classList.add('active');
-    questions[currentQuestionIndex].scrollIntoView({ behavior: 'smooth' });
-  }
-}
+// // Function to update the question index display
+// // function updateQuestionIndexDisplay() {
+// //   var questionIndex = currentQuestionIndex + 1;
+// //   var totalQuestions = questions.length;
+// //   questionIndexDisplay.textContent = 'Question ' + questionIndex + ' of ' + totalQuestions;
+// // }
 
-// Function to show the previous question
-function showPreviousQuestion() {
-  if (currentQuestionIndex > 0) {
-    questions[currentQuestionIndex].classList.remove('active');
-    currentQuestionIndex--;
-    questions[currentQuestionIndex].classList.add('active');
-    questions[currentQuestionIndex].scrollIntoView({ behavior: 'smooth' });
-  }
-}
+// // Function to show the next question
+// function showNextQuestion() {
+//   // if (currentQuestionIndex < questions.length - 1) {
+//     // questions[currentQuestionIndex].classList.remove('active');
+//     currentQuestionIndex++;
+//     // questions[currentQuestionIndex].classList.add('active');
+//     questions[currentQuestionIndex+1].scrollIntoView({ behavior: 'smooth' });
+//     // updateQuestionIndexDisplay();
+//   // }
+// }
+
+// // Function to show the previous question
+// function showPreviousQuestion() {
+//   // if (currentQuestionIndex > 0) {
+//     // questions[currentQuestionIndex].classList.remove('active');
+//     currentQuestionIndex--;
+//     // questions[currentQuestionIndex].classList.add('active');
+//     questions[currentQuestionIndex-1].scrollIntoView({ behavior: 'smooth' });
+//     // updateQuestionIndexDisplay();
+//   // }
+// }
 
 // Function to handle form submission
 function submitForm(event) {
   event.preventDefault();
   var name = document.getElementById('name').value;
-  var phone = document.getElementById('phone').value;
+  // var phone = document.getElementById('phone').value;
   var email = document.getElementById('email').value;
 
-  // Form validation
-  if (!name || !phone || !email) {
-    alert('Please fill out all fields.');
-    return;
-  }
+  // var confirmationMessage = document.getElementById('confirmation-message');
+  // confirmationMessage.innerHTML = 'Thank you, ' + name + ', for your submission! We will contact you at ' + phone + ' shortly.';
+
+  // // Form validation
+  // if (!name || !email) {
+  //   alert('Please fill out all fields.');
+  //   return;
+  // }
+
+  // alert("Thank you " + name + "! We will get back to you shortly at " + email)
 
   // Submit data to Google Sheets
-  var SCRIPT_ID = '1rKb3ZhvMwfdZZy_Y5TYl6DZNDTaWdJZkArQYmhYzy-gJingDXRKZFkAj';
-  var url = `https://script.google.com/macros/s/${SCRIPT_ID}/exec?name=${encodeURIComponent(name)}&phone=${encodeURIComponent(phone)}&email=${encodeURIComponent(email)}&sheet=RSVP`;
-  fetch(url)
-    .then(function(response) {
-      if (response.ok) {
-        showConfirmationPage(name, phone, email);
-      } else {
-        throw new Error('Error submitting form.');
-      }
-    })
-    .catch(function(error) {
-      console.error(error);
-      alert('Error submitting form. Please try again later.');
-    });
+  // var SCRIPT_ID = 'YOUR_SCRIPT_ID';
+  // var url = `https://script.google.com/macros/s/${SCRIPT_ID}/exec?name=${encodeURIComponent(name)}&phone=${encodeURIComponent(phone)}&email=${encodeURIComponent(email)}&sheet=RSVP`;
+  // fetch(url)
+  //   .then(function (response) {
+  //     if (response.ok) {
+  //       showConfirmationPage(name, phone, email);
+  //     } else {
+  //       throw new Error('Error submitting form.');
+  //     }
+  //   })
+  //   .catch(function (error) {
+  //     console.error(error);
+  //     alert('Error submitting form. Please try again later.');
+  //   });
+
+    // window.location.href = `/confirmation.html?name=${encodeURIComponent(name)}&phone=${encodeURIComponent(phone)}&email=${encodeURIComponent(email)}`;
+    redirectToConfirmationPage(name, phone, email)
+
+}
+
+function redirectToConfirmationPage(name, phone, email) {
+  var confirmationUrl = 'confirmation.html'; // Replace with the actual URL of the confirmation page
+  // Append query parameters to the URL if needed
+  confirmationUrl += `?name=${encodeURIComponent(name)}&phone=${encodeURIComponent(phone)}&email=${encodeURIComponent(email)}`;
+  window.location.href = confirmationUrl;
 }
 
 // Function to show confirmation page
-function showConfirmationPage(name, phone, email) {
-  window.location.href = `confirmation.html?name=${encodeURIComponent(name)}&phone=${encodeURIComponent(phone)}&email=${encodeURIComponent(email)}`;
-}
-
-// Function to copy color to clipboard
-function copyColor(color) {
-  navigator.clipboard.writeText(color);
-  alert("Copied the color: " + color);
-}
+// function showConfirmationPage(name, phone, email) {
+//   window.location.href = `/confirmation.html?name=${encodeURIComponent(name)}&phone=${encodeURIComponent(phone)}&email=${encodeURIComponent(email)}`;
+// }
 
 // Function to add event listeners
 function addEventListeners() {
-  nextButton.addEventListener('click', showNextQuestion);
-  prevButton.addEventListener('click', showPreviousQuestion);
-  form.addEventListener('submit', submitForm);
+  var nextButtons = document.querySelectorAll('.nextButton');
+  var prevButtons = document.querySelectorAll('.backButton');
+
+  nextButtons.forEach(function (button) {
+    button.addEventListener('click', showNextQuestion);
+  });
+
+  prevButtons.forEach(function (button) {
+    button.addEventListener('click', showPreviousQuestion);
+  });
+  // form.addEventListener('submit', submitForm);
 }
 
 // Function to handle Enter key press
-function handleEnterKeyPress(event) {
-  if (event.key === 'Enter') {
-    event.preventDefault();
-    if (event.shiftKey && currentQuestionIndex > 0) {
-      showPreviousQuestion();
-    } else if (currentQuestionIndex < questions.length - 1) {
-      showNextQuestion();
-    } else {
-      submitForm(event);
-    }
-  }
-}
+// function handleEnterKeyPress(event) {
+//   if (event.key === 'Enter') {
+//     // event.preventDefault();
+//     // // if (event.shiftKey && currentQuestionIndex > 0) {
+//     // if (event.shiftKey) {
+//     //   showPreviousQuestion();
+//     // // } else if (currentQuestionIndex < questions.length - 1) {
+//     // } else if (questions[currentQuestionIndex+1]) {
+//     //   showNextQuestion();
+//     // } else {
+//       submitForm(event);
+//     // }
+//   }
+// }
 
-// Initialize the script
-document.addEventListener('keydown', handleEnterKeyPress);
+// // Initialize the script
+// document.addEventListener('keydown', handleEnterKeyPress);
 addEventListeners();
+// updateQuestionIndexDisplay();
